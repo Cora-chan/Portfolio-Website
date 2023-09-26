9df35c5adf1e98c4147050f552b1a26d9c6e3249
Yue <kangyue9323@gmail.com>
2023-09-27 01:21:05 +1000

    nav finish; usestate pending

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Navbar from "./components/Navbar";
+
+
 function App() {
   return(
     <div>
-      <h1 className="text-2xl">hello orld</h1>
+      <Navbar />
       </div>
   );
-
 }
 export default App;
